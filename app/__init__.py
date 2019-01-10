25a3e4022dbcd0613718dece3f9cb2238888a467
kburudi <bkevery@gmail.com>
2019-01-10 03:37:34 +0300

    [feature #163097195]fix blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,7 @@ from flask import Flask, Blueprint
 from instanse.config import app_config
 from app.api.v1.views.user_views import v1_mod
 
-
-def my_app(config_name):
-    app = Flask(__name__)
-
-    app.config.from_object(app_config[config_name])
-
-    app.register_blueprint(v1_mod, url_prefix="/api/v1")
-
-    return app
+my_app = Flask(__name__)
+# my_app.config.from_object(app_config["development"])
+my_app.config['SECRET_KEY'] = "uudye78tgde6refs55w7iwtsj"
+my_app.register_blueprint(v1_mod, url_prefix="/api/v1")
