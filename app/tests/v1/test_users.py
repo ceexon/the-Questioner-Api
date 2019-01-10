918f9b1909a4311340797ce6abd46bc8328ca077
kburudi <bkevery@gmail.com>
2019-01-10 03:37:09 +0300

    [feature #163097195]update user tests

--- app/tests/v1/test_users.py
+++ app/tests/v1/test_users.py
@@ -37,8 +37,17 @@ class BaseTest(unittest.TestCase):
             "password": "$$22BBkk",
         }
 
+        # username missing
+        self.big_miss = {
+            "firstName": "Trevor",
+            "lastName": "Kurland",
+            "email": "trevbk@gmail.com",
+            "phone": "+254712345678",
+            "password": "$$22BBkk",
+        }
+
         self.user_data_return = {
-            "id" : 1,
+            "id": 1,
             "firstName": "Trevor",
             "lastName": "Kurland",
             "otherName": "Burudi",
@@ -46,8 +55,8 @@ class BaseTest(unittest.TestCase):
             "email": "trevbk@gmail.com",
             "phone": "+254712345678",
             "password": "$$22BBkk",
-            "regDate" : time_now.strftime("%D"),
-            "isAdmin" : True
+            "regDate": time_now.strftime("%D"),
+            "isAdmin": True
         }
 
         # missing confirmm password
@@ -115,61 +124,68 @@ class BaseTest(unittest.TestCase):
 class Testusers(BaseTest):
     """Tests user signup and login"""
 
-    def test_signup_success(self):
+    def test_signup_successful(self):
         response = self.client.post(
             '/api/v1/signup', data=json.dumps(self.usercreate1), content_type="application/json")
-        json_response = json.loads(response.get_data(as_text=True))
         self.assertEqual(response.status_code, 201)
-        self.assertEqual(json_response["data"], self.user_data_return )
 
     def test_missing_key_fields(self):
-        response = self.client.post('/api/v1/signup', data=json.dumps(self.usercreate7), content_type="application/json")
-        sign_resp = json.loads(response.get_data(as_text=True))
+        response = self.client.post(
+            '/api/v1/signup', data=json.dumps(self.big_miss), content_type="application/json")
+        sign_resp = json.loads(response.data.decode(
+            'utf-8', my_app.config['SECRET_KEY']))
         self.assertEqual(sign_resp["error"], "a key field is missing")
         self.assertEqual(response.status_code, 400)
 
     def test_empty_major_field(self):
-        response = self.client.post('/api/v1/signup', data=json.dumps(self.usercreate5), content_type="application/json")
-        sign_resp = json.loads(response.get_data(as_text=True))
-        self.assertEqual(sign_resp["error"], "all fields are required(firstName,lastName,password,userName,phone and email)")
+        response = self.client.post(
+            '/api/v1/signup', data=json.dumps(self.no_username), content_type="application/json")
+        sign_resp = json.loads(response.data.decode(
+            'utf-8', my_app.config['SECRET_KEY']))
+        self.assertEqual(
+            sign_resp["error"], "all fields are required(firstName,lastName,password,userName,phone and email)")
         self.assertEqual(response.status_code, 422)
 
     def test_username_taken(self):
-        response = self.client.post('/api/v1/signup', data=json.dumps(self.usernametaken), content_type="application/json")
-        sign_resp = json.loads(response.get_data(as_text=True))
-        self.assertEqual(sign_resp["error"], "user with that name already exists")
+        response = self.client.post(
+            '/api/v1/signup', data=json.dumps(self.usernametaken), content_type="application/json")
+        sign_resp = json.loads(response.data.decode(
+            'utf-8', my_app.config['SECRET_KEY']))
+        self.assertEqual(sign_resp["error"],
+                         "user with that name already exists")
         self.assertEqual(response.status_code, 409)
 
     def test_no_signup_data(self):
-        response = self.client.post('/api/v1/signup', data=json.dumps(self.nodata), content_type="application/json")
-        sign_resp = json.loads(response.get_data(as_text=True))
-        self.assertEqual(sign_resp["error"], "No data found")
+        response = self.client.post(
+            '/api/v1/signup', data=json.dumps(self.nodata), content_type="application/json")
         self.assertEqual(response.status_code, 204)
 
     def test_user_login_username_success(self):
         response = self.client.post(
             '/api/v1/login', data=json.dumps(self.userlogin1), content_type="application/json")
-        json_response = json.loads(response.get_data(as_text=True))
+        json_response = json.loads(response.data.decode("utf-8"))
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(json_response["data"], "logged in successfully with username")
+        self.assertEqual(json_response["data"],
+                         "logged in successfully with username")
 
     def test_user_login_email_success(self):
         response = self.client.post(
             '/api/v1/login', data=json.dumps(self.userlogin2), content_type="application/json")
-        json_response = json.loads(response.get_data(as_text=True))
+        json_response = json.loads(response.data.decode("utf-8"))
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(json_response["data"], "logged in successfully with email")
+        self.assertEqual(json_response["data"],
+                         "logged in successfully with email")
 
     def test_user_login_fail_user_email(self):
         response = self.client.post(
             '/api/v1/login', data=json.dumps(self.userlogin1), content_type="application/json")
-        json_response = json.loads(response.get_data(as_text=True))
+        json_response = json.loads(response.data.decode("utf-8"))
         self.assertEqual(response.status_code, 401)
         self.assertEqual(json_response["data"], "logged in failed")
 
     def test_empty_login_detail(self):
         response = self.client.post(
             '/api/v1/login', data=json.dumps(self.userlogin1), content_type="application/json")
-        json_response = json.loads(response.get_data(as_text=True))
+        json_response = json.loads(response.data.decode("utf-8"))
         self.assertEqual(response.status_code, 401)
         self.assertEqual(json_response["data"], "please fill all fields")
