511c6856973afbf5d16264fb9ed287118dc64ee1
kburudi <bkevery@gmail.com>
2019-01-13 09:18:24 +0300

    [bug #163123317]update blueprint and secret_key

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,15 @@
+import os
 from flask import Flask, Blueprint
 from instanse.config import app_config
-from app.api.v1.views.user_views import v1_mod
-from app.api.v1.views.meet_views import v1_mod
-from app.api.v1.views.question_views import v1_mod
+from app.api.v1.views.user_views import V1_MOD
+from app.api.v1.views.meet_views import V1_MOD
+from app.api.v1.views.question_views import V1_MOD
 
 
 def create_app(name_conf):
     my_app = Flask(__name__)
     # my_app.config.from_object(app_config["development"])
-    my_app.config['SECRET_KEY'] = "uudye78tgde6refs55w7iwtsj"
-    my_app.register_blueprint(v1_mod, url_prefix="/api/v1")
+    my_app.config['SECRET_KEY'] = os.getenv("SECRET")
+    my_app.register_blueprint(V1_MOD, url_prefix="/api/v1")
 
     return my_app
