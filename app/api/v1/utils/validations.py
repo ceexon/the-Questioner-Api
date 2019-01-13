f4945731f7aeadeaf4eb7675bc1230ccab142dbe
kburudi <bkevery@gmail.com>
2019-01-13 09:20:00 +0300

    [bug #163123317]modularised user validations

--- app/api/v1/utils/validations.py
+++ app/api/v1/utils/validations.py
@@ -1,165 +1,94 @@
-from flask import abort, make_response
-from app.api.v1.models.models import Users
+""" Validation for data received from a user """
+
+import os
 import re
-import datetime
+from functools import wraps
+import jwt
+from werkzeug.security import check_password_hash
+from flask import abort, make_response, jsonify, request
+from ..models.models import USER_LIST, UserModels
 
-time_now = datetime.datetime.now()
 EMAIL_REGEX = re.compile(r'(\w+[.|\w])*@(\w+[.])*\w+')
+PASSWORD_RE = re.compile('^(?=.*[!$?])(?=.*[a-z])(?=.*[A-Z]).{8}$')
 
 
-class UserValidation:
+class UserValidation():
     """ THis class validates all user data input from a user for signup and login """
 
-    def __init__(self, data_to_validate):
-        self.data = data_to_validate
+    correct_details = []
 
-    # validating for signup
-    def all_required_fields_signup(self):
-        ""
-        try:
-            self.fname = self.data["firstName"]
-            self.lname = self.data["lastName"]
-            self.username = self.data["userName"]
-            self.email = self.data["email"]
-            self.phone = self.data["phone"]
-            self.password = self.data["password"]
-
-        except:
-            return 0
-
-        return 1
-
-    def empty_fields_signup(self):
-        missing = ""
-        if not self.fname:
-            missing = "first name"
-        elif not self.lname:
-            missing = "last name"
-        elif not self.phone:
-            missing = "phone number"
-        elif not self.username:
-            missing = "username"
-        elif not self.email:
-            missing = "email"
-        elif not self.password:
-            missing = "password"
-
-        return missing + " cannot be empty!!"
+    def __init__(self, user_data):
+        """ set parameter to be passed whet calling the object """
+        self.data = user_data
 
     def valid_username(self):
-        if re.search("[!@#$%^&*-/\\')(;\"`<>?:|}{~ ]", self.username):
-            return 0
-        return 1
-
-    def username_exists(self):
-        if len(Users) == 0:
-            pass
-
-        for user in Users:
-            if self.username == user["userName"]:
-                return 1
-
-        return 0
+        """ validates the username parsed """
+        if re.search("[!@#$%^&*-/\\')(;\"`<>?:|}{~ ]", self.data["userName"]):
+            abort(make_response(
+                jsonify({"status": 400, "error": "username can only be a letter or _"}), 400))
 
     def valid_email(self):
-        email = self.email
-        if not EMAIL_REGEX.match(email):
-            return 0
-        return 1
+        """ validates the email parsed """
+        if not EMAIL_REGEX.match(self.data["email"]):
+            abort(make_response(
+                jsonify({"status": 400, "error": "invalid email format!!"}), 400))
 
     def valid_password(self):
-        message = ""
-        while True:
-            if len(self.password) < 6:
-                message = "password is short password"
-                break
-            elif not re.search("[a-z]", self.password):
-                message = "password has no lowercase letter"
-                break
-            elif not re.search("[a-z]", self.password):
-                message = "password has no lowercase letter"
-                break
-            elif not re.search("[A-Z]", self.password):
-                message = "password has no uppercase letter"
-                break
-            elif not re.search("[0-9]", self.password):
-                message = "password has no number"
-                break
-            elif not re.search("[*#@$]", self.password):
-                message = "password has no special character(*@#$)"
-                break
-            else:
-                return 1
-
-        return message
-
-    def add_default_fields(self):
-        if len(Users) == 0:
-            self.data["id"] = 1
-            self.data["isAdmin"] = True
-        else:
-            most_recent = Users[-1]
-            most_recent = most_recent["id"]
-            new_id = most_recent + 1
-            self.data["id"] = new_id
-            self.data["isAdmin"] = False
-
-        self.data["regDate"] = time_now.strftime("%D")
-
-        return self.data
-
-    # Validating for login
-    def empty_fields_login(self):
-        missing = ""
-        if not self.userlog:
-            missing = "email/username"
-        elif not self.password:
-            missing = "password"
-
-        return missing + " cannot be empty!!"
-
-    def all_required_fields_login(self):
-        try:
-            self.userlog = self.data["userlog"]
-            self.password1 = self.data["password"]
-
-        except:
-            return "both username/email and password are required"
-
-        return 1
-
-    def email_exists(self):
-        if len(Users) == 0:
+        """ validates the password parsed """
+        symbols = '$!#$@*'
+        pwd = self.data["password"]
+        if (len(pwd) >= 6 and
+                any(c.isdigit() for c in pwd) and
+                any(c.isupper() for c in pwd) and
+                any(c in symbols for c in pwd) and
+                all(c.isalnum() or c in symbols for c in pwd)):
             pass
-
-        for user in Users:
-            if self.email == user["email"]:
-                return 1
-
-        return 0
-
-    def is_email_or_username(self):
-        using_to_login = ""
-        if re.search("[@]", self.userlog):
-            using_to_login = "email"
         else:
-            using_to_login = "username"
+            abort(make_response(jsonify(
+                {"status": 400, "error": "password should have number, upper and lower letters + a special character"}), 400))
 
-        if using_to_login == "email":
-            if self.email_exists():
-                return "email"
+    def check_signup_exists(self):
+        """ check if user signing up exists """
+        in_users = UserModels(USER_LIST, self.data)
+        if in_users.check_exists("userName", self.data["userName"]):
+            abort(make_response(jsonify(
+                {"status": 409, "error": "user with the username already exists"}), 409))
+        if in_users.check_exists("email", self.data["email"]):
+            abort(make_response(
+                jsonify({"status": 409, "error": "user with that email already exists"}), 409))
 
-        elif using_to_login == "username":
-            if self.username_exists():
-                return "userName"
+    def confirm_login(self, login_choice):
+        if "@" in login_choice:
+            login_choice = "email"
+        else:
+            login_choice = "userName"
 
-        return "user not found"
+        self.correct_details = [user for user in USER_LIST if self.data["userlog"]
+                                == user[login_choice] and check_password_hash(user["password"], self.data['password'])]
+        if self.correct_details == []:
+            error = "invalid " + login_choice + " or pasword!!"
+            abort(make_response(
+                jsonify({"status": 401, "error": error}), 401))
 
-    def password_true_for_user(self):
-        exists = self.is_email_or_username()
-        if exists == "userName" or exists == "email":
-            for user in Users:
-                if user[exists] == self.userlog:
-                    return "loggged in as " + self.userlog
 
-        return 0
+def token_required(func_tion):
+    """ to check authentication token"""
+    @wraps(func_tion)
+    def decorated(*args, **kwargs):
+        token = None
+
+        if "x-access-token" in request.headers:
+            token = request.headers['x-access-token']
+
+        if not token:
+            abort(make_response(jsonify({"message": "Token is missing"}), 401))
+
+        try:
+            data = jwt.decode(token, os.getenv("SECRET_KEY"))
+            current_user = data["userName"]
+
+        except ValueError:
+            return jsonify({"message": "Token is invalid or expired"}), 401
+
+        return func_tion(current_user, *args, **kwargs)
+    return decorated
