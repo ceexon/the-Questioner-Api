ee0ec2a428b00d1485fca4177b72b06d04ffb661
kburudi <bkevery@gmail.com>
2019-01-15 08:40:16 +0300

    [starts #163140087]prevent wrong url postman error

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, Blueprint
+from flask import Flask, Blueprint, jsonify
 from instanse.config import app_config
 from app.api.v1.views.user_views import V1_MOD
 from app.api.v1.views.meet_views import V1_MOD
@@ -11,5 +11,13 @@ def create_app(name_conf):
     # my_app.config.from_object(app_config["development"])
     my_app.config['SECRET_KEY'] = os.getenv("SECRET")
     my_app.register_blueprint(V1_MOD, url_prefix="/api/v1")
+
+    @my_app.errorhandler(404)
+    def page_not_found(error):
+        return jsonify({'error': 'Url not found', 'status': 404}), 404
+
+    @my_app.errorhandler(405)
+    def method_not_allowed(error):
+        return jsonify({'error': 'Method not allowed', 'status': 405}), 405
 
     return my_app
