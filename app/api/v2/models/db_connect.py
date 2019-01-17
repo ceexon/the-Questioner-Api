849f2c14b036c9160c7e0240492ca5fdc209d810
kburudi <bkevery@gmail.com>
2019-01-17 11:53:38 +0300

    [chore #163243540] update db settings

--- app/api/v2/models/db_connect.py
+++ app/api/v2/models/db_connect.py
@@ -1,14 +1,20 @@
 import os
 import psycopg2
-from psycopg2.extras import RealDictCursor
 db_user = os.getenv("DB_USER")
 db_pass = os.getenv("DB_PASSWORD")
 db_host = os.getenv("DB_HOST")
 db_name = os.getenv("DB_NAME")
+db_data = "user={} password={} host={} dbname={}".format(
+    db_user, db_pass, db_host, db_name)
 
 try:
-    conn = psycopg2.connect("user={} password={} host={} dbname={}".format(
-        db_user, db_pass, db_host, db_name))
+
+    def connect_db(db_info):
+        conn = psycopg2.connect(db_info)
+        cur = conn.cursor()
+        return conn, cur
+
+    conn, cur = connect_db(db_data)
 
     def create_users_table(cur):
         cur.execute(
@@ -22,7 +28,8 @@ try:
         email VARCHAR(60) NOT NULL,
         phone VARCHAR(15) NOT NULL,
         password VARCHAR(60) NOT NULL,
-        isAdmin BOOLEAN DEFAULT FALSE,
+        publicId VARCHAR(20) NOT NULL,
+        isAdmin BOOLEAN DEFAULT False,
         register_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
       )""")
 
@@ -87,20 +94,40 @@ try:
           voted_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
           );""")
 
+    def drop_table_if_exists(cur):
+        """ Removes all tables on app start so as to start working with no data """
+        drop_users = """ DROP TABLE IF EXISTS users """
+        drop_meetups = """ DROP TABLE IF EXISTS meetups """
+        drop_questions = """ DROP TABLE IF EXISTS questions """
+        drop_comments = """ DROP TABLE IF EXISTS comments """
+        drop_rsvp = """ DROP TABLE IF EXISTS rsvp """
+        drop_votes = """ DROP TABLE IF EXISTS votes """
+
+        for d_table in [drop_votes, drop_rsvp, drop_comments, drop_meetups, drop_questions, drop_users]:
+            cur.execute(d_table)
+            save()
+
+    def perform_query(query):
+        cur.execute(query)
+
+    def save():
+        conn.commit()
+
+    def close_connection():
+        cur.close()
+        conn.close()
+        print("database action complete")
+
     def main():
-        cur = conn.cursor(cursor_factory=RealDictCursor)
+        drop_table_if_exists(cur)
         create_users_table(cur)
         create_meetups_table(cur)
         create_rsvp_table(cur)
         create_questions_table(cur)
         create_comment_table(cur)
         create_votes_table(cur)
-
-        if(conn):
-            conn.commit()
-            cur.close()
-            conn.close()
-            print("database creation complete")
+        save()
+        close_connection()
 
 except (Exception, psycopg2.Error) as error:
     print("Error while connecting to PostgreSQL", error)
